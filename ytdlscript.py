388604c4b2a7a5b409dd906f86342163594d840d
antsareflying <seongmumoon@gmail.com>
2021-11-09 18:34:07 +0800

    changed to yt-dlp and added --download-archive option

--- ytdlscript.py
+++ ytdlscript.py
@@ -1,7 +1,6 @@
 import subprocess
 import sys
 import os
-import time
 
 def sortlength(e):
     return len(e)
@@ -12,18 +11,18 @@ if len(sys.argv) != 2:
 
 ytlink = sys.argv[1]
 
-ytdlcommand = f"youtube-dl --write-description --write-info-json --write-annotations --write-sub --write-thumbnail -f mp4 -o C:/users/seong/archive/public/videos/%(title)s.%(ext)s {ytlink}"
+ytdl_command = f"yt-dlp --write-description --write-info-json --write-annotations --write-sub --write-thumbnail --download-archive downloadedarchive.txt -f mp4 -o C:/users/seong/archive/public/videos/%(title)s.%(ext)s {ytlink}"
 
-download = subprocess.run(ytdlcommand, check=True)
+download = subprocess.run(ytdl_command, check=True)
 
 
 FILES_EXIST = True
 while FILES_EXIST:
     print("starting while loop")
-    filelist = os.listdir('.')
-    sortedfilelist = [i for i in filelist if os.path.isfile(i)]
+    filelist = os.listdir('C:/users/seong/archive/public/videos')
+    sortedfilelist = [i for i in filelist if os.path.isfile(i) and i is not "downloadedarchive.txt"]
     sortedfilelist.sort(key=sortlength)
-    if len(sortedfilelist) == 0:
+    if len(sortedfilelist) == 1 and sortedfilelist[0] == "downloadedarchive.txt":
         FILES_EXIST = False
         print("no more individual files left, exiting")
         exit()
@@ -38,27 +37,26 @@ while FILES_EXIST:
     if not os.path.isdir(mkdirpath):
         os.mkdir(mkdirpath)
         print("made dir")
-        time.sleep(2)
 
-        tobemovedlist = os.listdir('.')
+        tobemovedlist = os.listdir('C:/users/seong/archive/public/videos')
         print(tobemovedlist)
-        sortedtobemovedlist = [i for i in tobemovedlist if i.startswith(filenamewithoutext) and os.path.isfile(i)]
+        sortedtobemovedlist = [i for i in tobemovedlist if i.startswith(filenamewithoutext) and os.path.isfile(i) and i is not "downloadedarchive.txt"]
         sortedtobemovedlist.sort(key=sortlength)
         print(sortedtobemovedlist)
 
 
         for file in sortedtobemovedlist:
+            if file is "donwnloadedarchive.txt":
+                break
             print(file)
             print("starting for loop")
-            time.sleep(2)
             destpath = f"C:/users/seong/archive/public/videos/{filenamewithoutext}/{file}"
             os.rename("C:/users/seong/archive/public/videos/" + file, destpath)
             print("moved file")
 
     else:
-        removefilelist = os.listdir('.')
-        sortedremovefilelist = [i for i in removefilelist if filenamewithoutext in i and os.path.isfile(i)]
+        removefilelist = os.listdir('C:/users/seong/archive/public/videos')
+        sortedremovefilelist = [i for i in removefilelist if filenamewithoutext in i and os.path.isfile(i) and i is not "downloadedarchive.txt"]
         for j in sortedremovefilelist:
-            time.sleep(2)
             os.remove(j)
             print("removed file")
